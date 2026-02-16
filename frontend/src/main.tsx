63f1ad4b6959d25e6a157783e163597a827b9fe3
Ryoma <altair20030916@gmail.com>
2026-02-16 11:24:23 +0900

    ［FE-2］Solo.tsxとSolo.cssの作成．localhost/SoloでSolo.tsxに移行．

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Solo from './Solo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* localhost:5173 */}
+        <Route path="/" element={<App />} />
+        {/* localhost:5173/solo */}
+        <Route path="/solo" element={<Solo />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
