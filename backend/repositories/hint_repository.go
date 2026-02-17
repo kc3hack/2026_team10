d91bdf2e7bc00b271edb58e06600e14ffaa5d95f
yone <is0698pi@ed.ritsumei.ac.jp>
2026-02-17 18:14:28 +0900

    メモリでの保存部分を削除

--- backend/repositories/hint_repository.go
+++ backend/repositories/hint_repository.go
@@ -9,31 +9,6 @@ type IHintRepository interface {
 	CreateRound(answer string, hints []string) (*models.Round, error)
 }
 
-type HintMemoryRepository struct {
-	rounds []models.Round
-}
-
-func NewHintMemoryRepository(rounds []models.Round) IHintRepository {
-	return &HintMemoryRepository{rounds: rounds}
-}
-
-func (r *HintMemoryRepository) CreateRound(answer string, hints []string) (*models.Round, error) {
-	var maxID uint
-	for _, rd := range r.rounds {
-		if rd.ID > maxID {
-			maxID = rd.ID
-		}
-	}
-	newID := maxID + 1
-	round := models.Round{
-		Answer: answer,
-		Hints:  hints,
-	}
-	round.ID = newID
-	r.rounds = append(r.rounds, round)
-	return &r.rounds[len(r.rounds)-1], nil
-}
-
 type HintRepository struct {
 	db *gorm.DB
 }
