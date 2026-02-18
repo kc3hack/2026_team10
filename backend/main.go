d97289cac55cf4c5a08db73a54d1d732299be402
kubodayo <is0667fe@ed.ritsumei.ac.jp>
2026-02-18 16:11:56 +0900

    AI出力のエンドポイントをsoloに修正

--- backend/main.go
+++ backend/main.go
@@ -33,7 +33,6 @@ func main() {
 		})
 	})
 	r.POST("/solo", hintController.StartGame)
-	r.POST("/solo/ai", hintController.StartGame_AI)
 	r.GET("/solo/:id/answer", hintController.GetAnswer)
 	r.POST("/solo/:id/answer", hintController.CheckAnswer)
 	r.Run() // デフォルトで0.0.0.0:8080で待機します
