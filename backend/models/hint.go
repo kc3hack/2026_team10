289385071b49d76ad3a3fe61ce5ea4a3ec2dd09b
yone <is0698pi@ed.ritsumei.ac.jp>
2026-02-19 02:49:04 +0900

    [BE-36] DBにis_finishedを追加し、正解時にtrueにする

--- backend/models/hint.go
+++ backend/models/hint.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Round struct {
 	gorm.Model
-	Answers []string `gorm:"serializer:json;not null"`
-	Hints   []string `gorm:"serializer:json;not null"` // ヒントはJSON形式で保存
+	Answers    []string `gorm:"serializer:json;not null"` // 答えはJSON形式で保存
+	Hints      []string `gorm:"serializer:json;not null"` // ヒントはJSON形式で保存
+	IsFinished bool     `gorm:"not null;default:false"`
 }
