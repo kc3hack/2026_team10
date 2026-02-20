4a83a1a32e79bf18ddb9744312e8c88b6a4391f4
yone <is0698pi@ed.ritsumei.ac.jp>
2026-02-20 21:06:39 +0900

    [FE-46] 正解画面をカードレイアウトに変更

--- frontend/src/Result/Components/ThanksConfetti.tsx
+++ frontend/src/Result/Components/ThanksConfetti.tsx
@@ -1,24 +1,18 @@
 import { useWindowSize } from "react-use";
 import Confetti from "react-confetti";
-import "../Styles/Result.css";
 
 const ThanksConfetti = () => {
 	const { width, height } = useWindowSize();
 
 	return (
-		<div className="confetti-overlay" style={{ pointerEvents: "none" }}>
-			<Confetti
-				width={width}
-				height={height}
-				recycle={true}
-				numberOfPieces={500}
-				gravity={0.2}
-				style={{ position: "fixed", top: 0, left: 0 }}
-			/>
-			<div className="confetti-content">
-				<h1 className="seikai-text">正解</h1>
-			</div>
-		</div>
+		<Confetti
+			width={width}
+			height={height}
+			recycle={true}
+			numberOfPieces={500}
+			gravity={0.2}
+			style={{ position: "fixed", top: 0, left: 0, pointerEvents: "none" }}
+		/>
 	);
 };
 
